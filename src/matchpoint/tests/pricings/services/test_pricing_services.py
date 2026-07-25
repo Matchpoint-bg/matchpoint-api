c1c784e056d512dc2051653472d53f36859a130b
Corentin Dupriez <dupriez.c@icloud.com>
2026-07-25 11:37:18 +0300

    FEAT: Added more tests

--- src/matchpoint/tests/pricings/services/test_pricing_services.py
+++ src/matchpoint/tests/pricings/services/test_pricing_services.py
@@ -12,8 +12,9 @@ class TestPricingServices(APITestCase):
         self.court = Court.objects.create(
             name="test", club_id=self.club, is_indoor=False, is_lit=False
         )
-        prices = Prices.objects.create(
+        self.prices = Prices.objects.create(
             court=self.court,
+            weekday="Friday",
             time_start=datetime.time(hour=8),
             time_end=datetime.time(hour=9),
             price_per_30_minutes=4,
@@ -24,3 +25,22 @@ class TestPricingServices(APITestCase):
             datetime.time(hour=8), datetime.time(hour=9), 8
         )
         self.assertEqual(amt, 16)
+
+    def test_get_pricing_for_30_minutes_returns_price(self):
+        amt = PricingService().get_price_for_30_minutes(
+            weekday="Friday",
+            time_start=datetime.time(hour=8),
+            time_end=datetime.time(hour=8, minute=30),
+            court=self.court,
+        )
+        self.assertEqual(amt, 4)
+
+    def test_get_pricing_for_30_minutes_no_data_raises(self):
+        with self.assertRaises(Exception) as e:
+            PricingService().get_price_for_30_minutes(
+                weekday="Friday",
+                time_start=datetime.time(hour=8),
+                time_end=datetime.time(hour=9, minute=30),
+                court=self.court,
+            )
+            self.assertEqual(e.msg, "No price found for this data")
