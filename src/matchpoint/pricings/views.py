e4bf0f629b05610890995af61514c2a7c413f15d
Corentin Dupriez <dupriez.c@icloud.com>
2026-08-02 18:31:21 +0300

    Updated PricingViewset to only keep update and delete. Added permission classes

--- src/matchpoint/pricings/views.py
+++ src/matchpoint/pricings/views.py
@@ -1,11 +1,21 @@
 # Create your views here.
 
-from rest_framework.viewsets import ModelViewSet
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
+from clubs.permissions import IsClubEmployeeOrAdmin
 from pricings.models import Prices
-from pricings.serializers import PricesSerializer
+from pricings.serializers import CourtsPricesSerializer, PricesSerializer
 
 
-class PricingViewset(ModelViewSet):
+@extend_schema_view(
+    update=extend_schema(summary="Update a specific pricing"),
+    partial_update=extend_schema(summary="Update a specific pricing (partial update)"),
+    destroy=extend_schema(summary="Delete a specific pricing"),
+)
+class PricingViewset(GenericViewSet, DestroyModelMixin, UpdateModelMixin):
     queryset = Prices.objects.all()
-    serializer_class = PricesSerializer
+    serializer_class = CourtsPricesSerializer
+    permission_classes = [IsAuthenticated, IsClubEmployeeOrAdmin]
