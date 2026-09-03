7a7f37def78f07192e418b8b5709580707b66284
Corentin Dupriez <dupriez.c@icloud.com>
2026-09-03 11:35:31 +0300

    Fixed static files for admin

--- src/matchpoint/profiles/urls.py
+++ src/matchpoint/profiles/urls.py
@@ -1,6 +1,10 @@
-# from .views import GetProfileDetails
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import AvatarViewset
 
 
-urlpatterns = [
-    #     path("me/", GetProfileDetails.as_view())
-]
+router = DefaultRouter()
+router.register(r"avatar", AvatarViewset)
+
+urlpatterns = [path("<int:pk>/", include(router.urls))]
+
--- src/matchpoint/profiles/views.py
+++ src/matchpoint/profiles/views.py
@@ -1,4 +1,8 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework.decorators import parser_classes
 from rest_framework.mixins import CreateModelMixin
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.status import HTTP_201_CREATED
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.request import Request
@@ -6,13 +10,22 @@ from rest_framework.response import Response
 
 from profiles.models import Profile
 from profiles.serializers import AvatarSerializer
+from users.permissions import IsSelf
 
 
 class AvatarViewset(GenericViewSet, CreateModelMixin):
+    queryset = Profile.objects.all()
+    parser_classes = [MultiPartParser, FormParser]
+    permission_classes = [IsAuthenticated, IsSelf]
+
+    def get_serializer_class(self):
+        return AvatarSerializer
+
+    @extend_schema(request=AvatarSerializer, tags=["Users"])
     def create(self, request: Request, pk=None) -> Response:
         profile = Profile.objects.get(pk=pk)
-        serializer = AvatarSerializer(data=request.data)
+        serializer = self.get_serializer_class()(data=request.data)
         serializer.is_valid(raise_exception=True)
         profile.avatar = serializer.validated_data["avatar"]
         profile.save()
-        return Response(status=HTTP_201_CREATED, data=serializer.data)
+        return Response(status=HTTP_201_CREATED, data="Image uploaded successfully")
